2507e036d0e615ef4580cab851f95e39f43b6093
c17hawke <sunny.c17hawke@gmail.com>
2022-07-19 13:32:16 +0530

    added log function to it

--- src/__init__.py
+++ src/__init__.py
@@ -1,1 +1,10 @@
+import logging
+import os
 
+def logger():
+    return logging.basicConfig(
+    filename=os.path.join("logs", 'running_logs.log'), 
+    level=logging.INFO, 
+    format="[%(asctime)s: %(levelname)s: %(module)s]: %(message)s",
+    filemode="a"
+    )
