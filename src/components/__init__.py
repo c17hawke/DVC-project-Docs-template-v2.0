b8036ad41b2b4b35970e7452870811f3303de0f1
c17hawke <sunny.c17hawke@gmail.com>
2022-07-19 13:32:36 +0530

    component seprated from pipeline

---
+++ src/components/__init__.py
@@ -0,0 +1,4 @@
+# # use if needed
+# from src.components.training import *
+# from src.components.featurization import *
+# from src.components.eval import *
---
+++ src/components/example_component.py
@@ -0,0 +1,13 @@
+# import module
+
+class Model:
+    def __init__(self):
+        pass
+    def methods(self, x):
+        pass
+
+class Training:
+    def __init__(self):
+        pass
+    def methods(self):
+        pass
