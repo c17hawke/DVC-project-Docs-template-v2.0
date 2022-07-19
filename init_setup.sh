05f63452ede9b636006499757b368fe89ae5b107
SUNNY BHAVEEN CHANDRA <2016peb5126@mnit.ac.in>
2022-07-19 13:56:38 +0530

    update in init

--- init_setup.sh
+++ init_setup.sh
@@ -6,12 +6,6 @@ echo [$(date)]: "activate environment"
 source activate ./env
 echo [$(date)]: "install requirements"
 pip install -r requirements.txt
-echo [$(date)]: "export conda environment"
-conda env export > conda.yaml
-echo "# ${PWD}" > README.md
-echo [$(date)]: "first commit"
-git add .
-git commit -m "first commit"
 echo [$(date)]: "END"
 
 # to remove everything -
